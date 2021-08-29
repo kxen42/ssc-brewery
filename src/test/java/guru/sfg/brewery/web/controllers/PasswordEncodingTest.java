0e05f621ecfbef9ee3130bb52d65292eb81c2326
Karen Christenson <kchristenson@illuminateed.net>
2021-08-28 20:23:16 -0500

    Override Default Password Controller

--- src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
@@ -4,17 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.util.DigestUtils;
 
 /**
- * Demos DigestUtils and MD5
+ * Demos
+ * <ul>
+ *  <li>DigestUtils and MD5</li>
+ * <li>NoOpPasswordEncoder</li>
+ *</ul>
  */
 public class PasswordEncodingTest {
 
     static final String PASSWORD = "password";
 
     @Test
-    void hashingExample() {
+    void md5HashingExample() {
         String first = DigestUtils.md5DigestAsHex(PASSWORD.getBytes());
         System.out.println(first);
         String second = DigestUtils.md5DigestAsHex(PASSWORD.getBytes());
@@ -25,5 +30,12 @@ public class PasswordEncodingTest {
         String third = DigestUtils.md5DigestAsHex(salted.getBytes());
         System.out.println(third);
         assertNotEquals(first, third);
+    }
+
+    @Test
+    void noopExample() {
+        // NoOp encoding is plaintext
+        String encoded = NoOpPasswordEncoder.getInstance().encode(PASSWORD);
+        assertEquals(PASSWORD, encoded);
     }
 }
