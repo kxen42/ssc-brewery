7843081a6f78fcbfcde0f17b55177df10ba0de1f
Karen Christenson <kchristenson@illuminateed.net>
2021-08-30 15:55:20 -0500

    MD5, SHA256, LDAP Password Encoders

--- src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
@@ -1,10 +1,13 @@
 package guru.sfg.brewery.web.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.util.DigestUtils;
 
 /**
@@ -12,6 +15,8 @@ import org.springframework.util.DigestUtils;
  * <ul>
  *  <li>DigestUtils and MD5</li>
  * <li>NoOpPasswordEncoder</li>
+ * <li>LdapShaPasswordEncoder</li>
+ * <li>StandardPasswordEncoder</li>
  *</ul>
  */
 public class PasswordEncodingTest {
@@ -19,16 +24,42 @@ public class PasswordEncodingTest {
     static final String PASSWORD = "password";
 
     @Test
+    void sha256Example() {
+        PasswordEncoder sha256 = new StandardPasswordEncoder();
+        String first = sha256.encode(PASSWORD);
+        System.out.printf("XENSON ---- 1st\t%s%n", first);
+
+        String encoded = sha256.encode(PASSWORD);
+        System.out.printf("XENSON ---- 2nd\t%s%n", encoded);
+
+        assertTrue(sha256.matches(PASSWORD, encoded));
+
+    }
+
+    @Test
+    void ldapShaExample() {
+        PasswordEncoder ldap = new LdapShaPasswordEncoder();
+        String first = ldap.encode(PASSWORD);
+        System.out.println("XENSON ---- ldap sha 1st " + first);
+        String second = ldap.encode(PASSWORD);
+        System.out.println("XENSON ---- ldap sha 2nd " + second);
+
+        String encodedPwd = ldap.encode(PASSWORD);
+        Assertions.assertTrue(ldap.matches(PASSWORD, encodedPwd));
+
+    }
+
+    @Test
     void md5HashingExample() {
         String first = DigestUtils.md5DigestAsHex(PASSWORD.getBytes());
-        System.out.println(first);
+        System.out.println("XENSON ---- MD5 1st not salted " + first);
         String second = DigestUtils.md5DigestAsHex(PASSWORD.getBytes());
-        System.out.println(second);
+        System.out.println("XENSON ---- MD5 2nd not salted " + second);
         assertEquals(first, second);
 
         String salted = PASSWORD + "ThisIsMySALTVALUE";
         String third = DigestUtils.md5DigestAsHex(salted.getBytes());
-        System.out.println(third);
+        System.out.println("XENSON ---- MD5 salted " + third);
         assertNotEquals(first, third);
     }
 
@@ -36,6 +67,7 @@ public class PasswordEncodingTest {
     void noopExample() {
         // NoOp encoding is plaintext
         String encoded = NoOpPasswordEncoder.getInstance().encode(PASSWORD);
+        System.out.println("XENSON ---- NoOp encoded " + encoded);
         assertEquals(PASSWORD, encoded);
     }
 }
